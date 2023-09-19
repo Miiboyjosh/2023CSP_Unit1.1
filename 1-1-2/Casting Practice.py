867d5cd1544d72a42e065f4017b1e195fff67574
Joshua <joshua.jean@vtsd.com>
2023-09-19 10:06:56 -0400

    9/19/23

---
+++ 1-1-2/Casting Practice.py
@@ -0,0 +1,15 @@
+pottle = 1.2
+lottle = 130
+mii = "boy"
+
+print("Initial Values:")
+print("pottle:", pottle)
+print("lottle:", lottle)
+print("mii:", mii)
+
+lottle = chr(lottle)
+
+print("Ending Values:")
+print("pottle:", pottle)
+print("lottle:", lottle)
+print("mii:", mii)
