435ec55c34ad96a0e08300360ec45a7875b035c9
Joshua <joshua.jean@vtsd.com>
2023-10-11 08:41:29 -0400

    9/19/23

--- 1-1-6/step5.py
+++ 1-1-6/step5.py
@@ -3,18 +3,42 @@ import turtle as trtl
 # instead of a descriptive name of the turtle such as painter,
 # a less useful variable name x is used
 spider = trtl.Turtle()
+
+# Create a Spider Body
+
 spider.pensize(40)
 spider.circle(20)
+
+# Configure Spider Legs
+
 legs = 8
-lengthOfLegs = 100
-spacing = 380 / legs
+lengthOfLegs = 80
+spacing = 180 / legs
 spider.pensize(5)
 n = 0
+
+# Draw Spider Legs
+
 while (n < legs):
-  spider.goto(0,0)
-  spider.setheading(spacing * n)
-  spider.forward(lengthOfLegs)
-  n = n + 1
+  if (n > 3):
+  # Draw First Half of Legs
+    spider.goto(0,20)
+    spider.setheading(spacing * n)
+    spider.forward(lengthOfLegs)
+    n = n + 1
+  else:
+  # Draw Second Half of Legs
+    spider.goto(0,20)
+    spider.setheading(spacing * -n)
+    spider.forward(lengthOfLegs)
+    n = n + 1
+
+# Draw Spider Eyes
+
+while ()
+spider.pensize(40)
+spider.circle(10)
+
 spider.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
