826b8dfe2e0b243b9cd41ffcd2743d7c03d90583
Joshua <joshua.jean@vtsd.com>
2023-10-24 08:40:21 -0400

    9/19/23

---
+++ 1-1-8/step10.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number -> "))
+if (num <= 64):
+  print("Grade is a D")
+elif (num <= 70):
+  print("Grade is a C")
+elif (num <= 80):
+  print("Grade is a B")
+elif (num <= 90):
+  print("Grade is a A")
