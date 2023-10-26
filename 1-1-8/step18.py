717b923b332af56ae5c1ad3e66d596a88f909f2d
Joshua <joshua.jean@vtsd.com>
2023-10-26 10:07:29 -0400

    9/19/23

---
+++ 1-1-8/step18.py
@@ -0,0 +1,54 @@
+#   a118_turtles_in_traffic.py
+#   Move turtles horizontally and vertically across screen.
+#   Stopping turtles when they collide.
+import turtle as trtl
+
+# create two empty lists of turtles, adding to them later
+horiz_turtles = []
+vert_turtles = []
+
+# use interesting shapes and colors
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+horiz_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown"]
+
+#Turtle Location
+tloc = 50
+#Brings in the Turtles
+for s in turtle_shapes:
+    ht = trtl.Turtle(shape=s)
+    horiz_turtles.append(ht)
+    ht.penup()
+    new_color = horiz_colors.pop()
+    ht.fillcolor(new_color)
+    ht.goto(-350, tloc)
+    ht.speed(0)
+    ht.setheading(0)
+
+    vt = trtl.Turtle(shape=s)
+    vert_turtles.append(vt)
+    vt.penup()
+    new_color = vert_colors.pop()
+    vt.fillcolor(new_color)
+    vt.goto(-tloc, 350)
+    vt.speed(0)
+    vt.setheading(270)
+
+    tloc += 50
+
+# TODO: move turtles across and down screen, stopping for collisions
+#
+
+for steps in range(50):
+    for ht in horiz_turtles:
+        for vt in vert_turtles:
+            ht.forward(3)
+            vt.forward(3)
+            if (abs(x1 - x2) >= 20)
+                horiz_turtles.
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
